57494c6e35a5e95c0c85ac01776752128df55847
slince-zero <1711257704@qq.com>
2023-07-05 18:55:34 +0800

    初始化

---
+++ src/components/Student.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>学生姓名: {{ name }}</h2>
+        <h2>年龄: {{ age }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data() {
+        return {
+            name: '小明',
+            age: '22'
+        }
+    }
+}
+</script>
+
+<style></style>
